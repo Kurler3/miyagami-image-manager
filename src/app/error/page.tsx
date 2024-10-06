543f3a50f0dc55d98292347097b441fa0a34d2f6
Kurler3 <miguelbento2000@gmail.com>
2024-10-06 16:46:26 +0200

    actually fixed

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,16 +1,27 @@
 'use client'
 
 import { useSearchParams } from "next/navigation"
+import { Suspense } from 'react'
 
-export default function ErrorPage() {
 
+const ErrorComponent = () => {
     const searchParams = useSearchParams()
     const msg = searchParams.get('msg') ?? 'An unknown error occurred.'
 
     return (
-        <div>
-            <h1>Error</h1>
-            <p>{msg}</p>
-        </div>
+            <div>
+                <h1>Error</h1>
+                <p>{msg}</p>
+            </div>
+        
+    )
+}
+
+export default function ErrorPage() {
+
+    return (
+        <Suspense>
+            <ErrorComponent />
+        </Suspense>
     )
 }
